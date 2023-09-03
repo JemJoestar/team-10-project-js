16a300d6c81fa5ef5e81ed017d670c51ae8dd3fd
Malvina1993 <mykhamalvina@gmail.com>
2023-09-03 16:36:36 +0300

    change files bu theme, added localStorage

--- src/partials/theme/theme.js
+++ src/partials/theme/theme.js
@@ -1,15 +1,24 @@
 const btnChange = document.querySelector('.checkbox-ios');
 
-const THEME = {
-    light: 'light',
-    dark: 'dark',
+const THEME = 'dark';
+
+// перевірка, чи є у сxовищі тема 
+if (!(localStorage.getItem('theme') === null)) {
+    document.body.classList.add(THEME);
 }
 
-
+// додаємо слухач на перемикач теми
 btnChange.addEventListener('change', onChangeThemeClick);
 
-function onChangeThemeClick(e, {light,dark} = THEME) {
-    document.body.classList.toggle(light);
-    document.body.classList.toggle(dark);
+function onChangeThemeClick() {
+    document.body.classList.toggle(THEME);
+//  Перевіряємо, чи додана тема. Якщо так, додаємо у веб сховище 
+    if (document.body.classList.contains(THEME)) {
+        localStorage.setItem('theme', THEME);
+        return;
+    }
+
+    localStorage.removeItem('theme');
 }
+
 
