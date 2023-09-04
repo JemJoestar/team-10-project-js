e8b592381eb403049d67d26cff824557bdd1e0fb
JemJoestar <jemapro2@gmail.com>
2023-09-04 22:14:00 +0300

    add responsive shop list

--- src/partials/shopping-list/shopping-list.js
+++ src/partials/shopping-list/shopping-list.js
@@ -1,37 +1,55 @@
-import trashSvg from "../../images/icon.svg"
-import appleBook from "./images/apple-book.png"
-import amazonLogo from "./images/amazon-logo.png"
-import bookShop from "./images/book-shop.png"
-
-
+import trashSvg from '../../images/icon.svg';
+import appleBook from './images/apple-book.png';
+import amazonLogo from './images/amazon-logo.png';
+import bookShop from './images/book-shop.png';
 
 const section = document.querySelector('.shopping-list-js');
 
 const bookCards = JSON.parse(localStorage.getItem('bookShopList'));
-console.log(`bookCards:`, bookCards)
+console.log(`bookCards:`, bookCards);
 
 function createMarkup() {
   return bookCards.reduce(
-    (markup, { author, book_image, title, description, list_name}) => {
+    (
+      markup,
+      { author, book_image, title, description, list_name, buy_links, _id }
+    ) => {
       return (
         markup +
-        `<div class="card-book">
-    <button class="btn-delete" type="button">
+        `<div class="card-book" data-id="${_id}">
+    <b class="btn-delete" type="button">
     <svg class="delete" width="12" height="12">
     <use href="${trashSvg}#icon-trash"></use>
     </svg>
-    </button>
+    </b target="_blank"utton>
     <img
       class="poster"
       src="${book_image}"
       alt="poster"
       />
-      <div>
+      <div class="book-inform">
       <h2 class="book-title">${title}</h2>
       <p class="book-undertitle">${list_name}</p>
       <p class="book-text">${description}</p>
       <div class="card-undertext">
       <p class="author">${author}</p>
+      <div class="shop-list">
+        <a href="${
+          buy_links.find(link => link.name === 'Amazon').url
+        }" class="amazon-logo  shopping-list-logo" target="_blank">
+        <img src="${amazonLogo}" height="11">
+        </a>
+        <a href="${
+          buy_links.find(link => link.name === 'Apple Books').url
+        }" class="apple-logo shopping-list-logo" target="_blank">
+        <img src="${appleBook}" height="16">
+        </a>
+        <a href="${
+          buy_links.find(link => link.name === 'Bookshop').url
+        }" class="book-logo shopping-list-logo" target="_blank">
+        <img src="${bookShop}" height="16">
+        </a>
+      </div>
       </div>
       </div>
       </div>`
@@ -42,11 +60,11 @@ function createMarkup() {
 }
 // console.log(createMarkup(bookCards))
 
-try{
-
-  if(bookCards.length != 0){
-    section.innerHTML = ""
+try {
+  if (bookCards.length != 0) {
+    section.innerHTML = '';
   }
   section.insertAdjacentHTML('beforeend', createMarkup(bookCards));
-
-}catch(err){}
+} catch (err) {
+  console.log(`err:`, err);
+}
